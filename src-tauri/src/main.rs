eae91c4bae5528598a615dc114c4d66a46551f48
Christopher Angelo <angelo@angeloanan.xyz>
2024-01-31 05:51:46 +0700

    feat: hourly notification
    * remove `lazy-static` crate and uses `std::sync::LazyLock`
    * Added notification sound
    * Plays notification sound on actions
    * Spawn `tokio::task`s on actions so to not block main thread
    * Notify every 1 hour
    * Add extra button to open settings

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,26 @@
+#![feature(lazy_cell)]
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::fmt::format;
+use std::sync::LazyLock;
 
 use directories::ProjectDirs;
-use lazy_static::lazy_static;
+use reqwest::Url;
+use rodio::{Decoder, OutputStream, Sink};
 use tauri::{
-    api::notification::{Notification, Sound},
-    App, AppHandle, CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, Window,
-    WindowBuilder,
+    api::notification::Notification, AppHandle, CustomMenuItem, Manager, SystemTray,
+    SystemTrayEvent, SystemTrayMenu, WindowBuilder,
 };
+use tokio::time;
+
+static PROJECT_IDENTIFIER: &'static str = "fyi.angelo.hydrate-reminder";
+static PROJECT_DIR: LazyLock<ProjectDirs> =
+    LazyLock::new(|| ProjectDirs::from("fyi", "angelo", "hydrate-reminder").unwrap());
+
+static NOTIFICATION_AUDIO: LazyLock<&'static [u8]> =
+    LazyLock::new(|| include_bytes!("../assets/notif.mp3"));
+static DRINK_AUDIO: LazyLock<&'static [u8]> =
+    LazyLock::new(|| include_bytes!("../assets/gulp.mp3"));
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -17,14 +28,19 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn create_new_window(app: &AppHandle) -> Window {
-    let window = WindowBuilder::new(app, "main", tauri::WindowUrl::App("index.html".into()))
-        .hidden_title(true)
-        .resizable(false)
-        .decorations(false)
-        .closable(true)
-        .build()
-        .expect("Unable to create a new window!");
+fn spawn_main_window(app: &AppHandle) {
+    if let Some(main_window) = app.get_window("main") {
+        main_window
+            .set_focus()
+            .expect("Unable to focus main window!")
+    } else {
+        WindowBuilder::new(app, "main", tauri::WindowUrl::App("index.html".into()))
+            .hidden_title(true)
+            .resizable(false)
+            .closable(true)
+            .build()
+            .expect("Unable to create a new window!");
+    }
 
     // window.on_window_event(|e| {
     //     match e {
@@ -36,30 +52,46 @@ fn create_new_window(app: &AppHandle) -> Window {
 
     //     println!("Window event: {:?}", e);
     // });
-
-    window
 }
 
 #[tauri::command]
 fn create_drink_notification(app: AppHandle) {
-    println!("Creating drink notification");
-    let notif_sound = Sound::Default;
+    tokio::spawn(async move {
+        let audio_buffer = std::io::Cursor::new(*NOTIFICATION_AUDIO);
+        let audio = Decoder::new(audio_buffer).unwrap();
 
-    Notification::new(&app.config().tauri.bundle.identifier)
-        .title("Time to drink!")
-        .body("It's been 1 hour since your last drink, time to drink again!")
-        .sound(notif_sound)
-        .show()
-        .expect("Unable to create drink notification!");
+        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+        let sink = Sink::try_new(&stream_handle).unwrap();
+
+        sink.append(audio);
+        Notification::new(&app.config().tauri.bundle.identifier)
+            .title("Time to drink!")
+            .body("It's been 1 hour since your last drink, time to drink again!")
+            .show()
+            .expect("Unable to create drink notification!");
+
+        sink.sleep_until_end();
+    });
+}
+
+fn play_drink_sound() {
+    tokio::spawn(async move {
+        let audio_buffer = std::io::Cursor::new(*DRINK_AUDIO);
+        let audio = Decoder::new(audio_buffer).unwrap();
+
+        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+        let sink = Sink::try_new(&stream_handle).unwrap();
+
+        sink.append(audio);
+        sink.sleep_until_end()
+    });
 }
 
 fn handle_tray_event(app: &AppHandle, event: SystemTrayEvent) {
     match event {
         tauri::SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
-            "drink" => {
-                println!("I have {} windows", app.windows().len());
-                create_new_window(app);
-            }
+            "drink" => play_drink_sound(),
+            "open-settings" => spawn_main_window(app),
 
             "quit" => app.exit(0),
 
@@ -98,31 +130,39 @@ async fn handle_oauth_flow(app: &AppHandle) {
         .expect("Unable to create a new window!");
 }
 
-lazy_static! {
-    static ref PROJECT_DIR: ProjectDirs =
-        ProjectDirs::from("fyi", "angelo", "hydrate-reminder").unwrap();
-}
-
 #[tokio::main]
 async fn main() {
     let tray_menu = SystemTrayMenu::new()
-        .add_item(CustomMenuItem::new("drink", "Drink"))
+        .add_item(CustomMenuItem::new("drink", "🥛 Drink"))
+        .add_item(CustomMenuItem::new("open-settings", "Open Settings"))
         .add_native_item(tauri::SystemTrayMenuItem::Separator)
         .add_item(CustomMenuItem::new("quit", "Quit"));
 
     let tray = SystemTray::new().with_menu(tray_menu);
 
-    tauri::Builder::default()
+    let mut app = tauri::Builder::default()
         .plugin(tauri_plugin_store::Builder::default().build())
         .system_tray(tray)
         .on_system_tray_event(handle_tray_event)
         .invoke_handler(tauri::generate_handler![create_drink_notification, greet])
         .build(tauri::generate_context!())
-        .expect("Error while running tauri application")
-        .run(|_, e| match e {
-            tauri::RunEvent::ExitRequested { api, .. } => {
-                api.prevent_exit();
-            }
-            _ => {}
-        });
+        .expect("Error while running tauri application");
+
+    app.set_activation_policy(tauri::ActivationPolicy::Accessory);
+
+    tokio::task::spawn(notification_task(app.app_handle()));
+
+    app.run(|_, e| match e {
+        tauri::RunEvent::ExitRequested { api, .. } => {
+            api.prevent_exit();
+        }
+        _ => {}
+    });
+}
+
+async fn notification_task(app: AppHandle) {
+    loop {
+        tokio::time::sleep(time::Duration::from_secs(60 * 60)).await;
+        create_drink_notification(app.clone())
+    }
 }
