dda49bdd23d688e4befb6904d5da1a58452a9caf
Christopher Angelo <angelo@angeloanan.xyz>
2024-02-05 10:13:10 +0700

    fix(capnproto): rebuild on schema change

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -4,5 +4,8 @@ fn main() {
         .run()
         .expect("schema compiler command");
 
-    tauri_build::build()
+    tauri_build::build();
+
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=schema/app.capnp");
 }
