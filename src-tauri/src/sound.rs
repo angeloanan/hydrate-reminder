4b3be39b9554c28ee4bdead5b4ccd82467a6cdaa
Christopher Angelo <angelo@angeloanan.xyz>
2024-03-08 11:48:58 +0700

    refactor(sound): uses `Decoder::new_mp3` to skip probing

--- src-tauri/src/sound.rs
+++ src-tauri/src/sound.rs
@@ -5,10 +5,10 @@ const DRINK_AUDIO: &[u8] = include_bytes!("../assets/gulp.mp3");
 
 pub fn notification_audio() -> Decoder<std::io::Cursor<&'static [u8]>> {
     let audio_buffer = std::io::Cursor::new(NOTIFICATION_AUDIO);
-    Decoder::new(audio_buffer).unwrap()
+    Decoder::new_mp3(audio_buffer).unwrap()
 }
 
 pub fn drink_audio() -> Decoder<std::io::Cursor<&'static [u8]>> {
     let audio_buffer = std::io::Cursor::new(DRINK_AUDIO);
-    Decoder::new(audio_buffer).unwrap()
+    Decoder::new_mp3(audio_buffer).unwrap()
 }
